731f30b168c7a1e24038479dcba47ad64d7aa72f
rakeshsfdcAi <grakeshsfdc@gmail.com>
2025-05-23 04:01:54 +0530

    Add new LWC component

---
+++ force-app/main/default/lwc/component2/component2.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class Component2 extends LightningElement {
+    customerCode = 'CUST001';
+    customName = 'John Doe';
+    emailAddress = 'test@gmail.com';
+    phoneNumber = '123456789';
+    streetAddress = '123 Main St';
+    city = 'San Jose';
+    state = 'CA';
+    zipCode = '12345';
+}
