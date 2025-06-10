f8d9c95a04e4115c50fa12d51c277c87ad808961
rakeshsfdcAi <grakeshsfdc@gmail.com>
2025-06-11 01:08:44 +0530

    imperative

---
+++ force-app/main/default/lwc/imperativeWay/imperativeWay.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import getaccount1 from '@salesforce/apex/getAccounts.getAccount';
+
+export default class ImperativeWay extends LightningElement {
+    recs1;
+    handleCLick() {
+        getaccount1().then(result => {
+            this.recs1 = result;
+        }).catch(error => {
+            console.error('Error fetching accounts:', error);
+            this.acc1 = undefined; // Clear the account data on error
+        }
+        );
+    }
+
+}
